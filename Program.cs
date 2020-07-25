39b9407d28b734298f4130377b6f7650e3646383
mikeychambers <49445856+mikeychambers@users.noreply.github.com>
2020-07-25 18:53:01 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+
+namespace PrankyPrank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread speachText = new Thread(SpeechText);
+            Thread mouseShake = new Thread(MouseShake);
+            Thread textWrite = new Thread(TextWrite);
+            speachText.Start();
+            textWrite.Start();
+            mouseShake.Start();
+        }
+        static void SpeechText()
+        {
+            while (true)
+            {
+                SpeechSynthesizer voice = new SpeechSynthesizer();
+                voice.Speak(Environment.UserName + " You have been pwned");
+            }
+        }
+
+        static void TextWrite()
+        {
+            while (true)
+            {
+                Console.WriteLine("Deleting {0} ", Environment.SystemDirectory);
+
+            }
+        }
+        static void MouseShake()
+        {
+            while (true)
+            {
+                Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y - 50);
+                Cursor.Position = new Point(Cursor.Position.X + 50, Cursor.Position.Y + 50);
+            }
+
+        }
+
+
+    }
+}
